80ed20a465cfa68598b08fc2300dee89ce73606d
Angel Gomez <feutet@gmail.com>
2022-03-28 22:08:16 -0400

    Add initial Firebase integration with places/reservations

--- src/routes/book.svelte
+++ src/routes/book.svelte
@@ -1,3 +1,49 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { addDoc, collection, getDocs, query, serverTimestamp } from 'firebase/firestore';
+  import { firestore } from '$lib/firebase';
+
+  let places: any;
+
+  // Form inputs.
+  let direction = 'from';
+  let fareType = 'one-way';
+  let placeId = '';
+  let address = '';
+
+  async function getPlaces() {
+    const q = query(collection(firestore, 'places'));
+
+    const snapshot = await getDocs(q);
+
+    let _places: any = [];
+
+    snapshot.forEach((doc) => {
+      _places.push({ id: doc.id, ...doc.data() });
+    });
+
+    console.log(_places);
+
+    return _places;
+  }
+
+  async function handleSubmit() {
+    await addDoc(collection(firestore, 'reservations'), {
+      direction,
+      fareType,
+      placeId,
+      address,
+      createdAt: serverTimestamp(),
+    });
+
+    alert('Saved');
+  }
+
+  onMount(() => {
+    places = getPlaces();
+  });
+</script>
+
 <svelte:head>
   <title>Book now</title>
 </svelte:head>
@@ -6,35 +52,60 @@
 
 <h1>Book now</h1>
 
-<form>
+<form on:submit|preventDefault={handleSubmit}>
   <div>
-    <input type="radio" id="direction-from" name="direction" value="from" />
+    <input type="radio" id="direction-from" name="direction" value="from" bind:group={direction} />
     <label for="direction-from">From SMA</label>
 
-    <input type="radio" id="direction-to" name="direction" value="to" />
+    <input type="radio" id="direction-to" name="direction" value="to" bind:group={direction} />
     <label for="direction-to">To SMA</label>
   </div>
 
   <div>
-    <input type="radio" id="fare-type-one-way" name="fare-type" value="one-way" />
+    <input
+      type="radio"
+      id="fare-type-one-way"
+      name="fare-type"
+      value="one-way"
+      bind:group={fareType}
+    />
     <label for="fare-type-one-way">One way</label>
 
-    <input type="radio" id="fare-type-round-trip" name="fare-type" value="round-trip" />
+    <input
+      type="radio"
+      id="fare-type-round-trip"
+      name="fare-type"
+      value="round-trip"
+      bind:group={fareType}
+    />
     <label for="fare-type-round-trip">Round trip</label>
   </div>
 
   <div>
-    <select>
-      <option value="">Select airport</option>
-      <option value="aiq">Aeropuerto Internacional de Queretaro (AIQ)</option>
-      <option value="bjx">Aeropuerto Internacaional del Bajio (BJX)</option>
-      <option value="aicm">Aeropuerto Internacional de Ciudad de Mexico (AICM)</option>
+    <select required value={placeId}>
+      {#await places}
+        <option value="">Loading...</option>
+      {:then result}
+        <option value="">Select airport</option>
+        {#if result}
+          {#each result as place (place.id)}
+            <option value={place.id}>
+              {place.name}
+              {#if place.code}
+                ({place.code})
+              {/if}
+            </option>
+          {/each}
+        {/if}
+      {:catch}
+        <option value="">Error</option>
+      {/await}
     </select>
   </div>
 
   <div>
     <label for="address">Address</label>
-    <input type="text" name="address" id="address" />
+    <input type="text" name="address" id="address" bind:value={address} required />
   </div>
 
   <button type="submit">Submit</button>
