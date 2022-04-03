1b942435928373d80cc8b1b9be84e40051552999
Angel Gomez <feutet@gmail.com>
2022-04-03 18:27:16 -0400

    Add reservations list page for admins

--- src/lib/components/ReservationItem.svelte
+++ src/lib/components/ReservationItem.svelte
@@ -1,16 +1,81 @@
-<script>
+<script lang="ts">
   export let reservation;
+  export let showTimestamp = false;
+  export let showPickupTime = false;
+  export let showTotal = false;
+
+  import dayjs from 'dayjs';
+  import dayjsUTC from 'dayjs/plugin/utc.js';
+  import dayjsTz from 'dayjs/plugin/timezone.js';
+  import dayjsRelativeTime from 'dayjs/plugin/relativeTime.js';
+  import { onMount } from 'svelte';
+
+  const TIMEZONE = 'America/Mexico_City';
+
+  function getDirectionText(str: string) {
+    if (str === 'to') {
+      return 'To SMA';
+    } else if (str === 'from') {
+      return 'From SMA';
+    }
+  }
+
+  function getFareTypeText(str: string) {
+    if (str === 'one-way') {
+      return 'One way';
+    } else if (str === 'round-trip') {
+      return 'Round trip';
+    }
+  }
+
+  onMount(() => {
+    dayjs.extend(dayjsUTC);
+    dayjs.extend(dayjsTz);
+    dayjs.extend(dayjsRelativeTime);
+
+    dayjs.tz.setDefault(TIMEZONE);
+  });
 </script>
 
 <a
   href={`/reservations/${reservation.id}`}
-  class="group rounded-md border border-slate-200 px-3 py-2 shadow-sm transition hover:shadow md:flex md:justify-between md:px-4 md:py-3"
+  class="group rounded-md border border-slate-200 px-3 py-2 shadow-sm transition hover:shadow md:flex md:items-center md:justify-between md:px-4 md:py-3"
 >
-  <p class="text-sm font-medium">ID: {reservation.id}</p>
+  <section class="flex grow flex-col">
+    <p class="text-sm font-semibold text-slate-700">ID: {reservation.id}</p>
+    <section class="mt-1 flex flex-wrap items-center">
+      {#if showTimestamp}
+        <span class="mr-2 text-xs text-slate-500">
+          {dayjs.unix(reservation.createdAt.seconds).tz(TIMEZONE).fromNow()}
+        </span>
+      {:else if showPickupTime}
+        <span class="mr-2 text-xs text-slate-500">
+          {dayjs.unix(reservation.pickupTime.seconds).tz(TIMEZONE).fromNow()}
+        </span>
+      {/if}
+      <span
+        class="mr-2 rounded-full border border-indigo-200 bg-indigo-50 py-1 px-2 text-xs font-semibold text-indigo-600"
+      >
+        {getDirectionText(reservation.direction)}
+      </span>
+      <span
+        class="mr-2 rounded-full border border-slate-200 bg-slate-50 py-1 px-2 text-xs font-semibold text-slate-600"
+      >
+        {getFareTypeText(reservation.fareType)}
+      </span>
+      {#if showTotal && reservation.totalCost}
+        <span
+          class="mr-2 rounded-full border border-green-200 bg-green-50 py-1 px-2 text-xs font-semibold text-green-600"
+        >
+          ${reservation.totalCost}
+        </span>
+      {/if}
+    </section>
+  </section>
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="currentColor"
-    class="hidden h-5 w-5 text-slate-400 md:group-hover:block"
+    class="hidden h-5 w-5 flex-shrink-0 text-slate-400 md:group-hover:block"
     viewBox="0 0 16 16"
   >
     <path
---
+++ src/routes/reservations/index.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+  import { collection, getDocs, orderBy, query, limit } from 'firebase/firestore';
+  import { goto } from '$app/navigation';
+  import { firestore } from '$lib/firebase';
+  import { authStore } from '$lib/stores/auth';
+  import ReservationItem from '$lib/components/ReservationItem.svelte';
+
+  // Redirect back to home if unauthenticated.
+  $: if (!$authStore.isLoading && !$authStore.user) {
+    goto('/');
+  }
+
+  async function getReservations() {
+    const q = query(collection(firestore, 'reservations'), orderBy('createdAt', 'desc'), limit(10));
+    const snap = await getDocs(q);
+
+    let _reservations: any[] = [];
+
+    snap.forEach((doc) => {
+      _reservations.push({ id: doc.id, ...doc.data() });
+    });
+
+    return _reservations;
+  }
+
+  let reservationsPromise;
+
+  $: if (!reservationsPromise && !$authStore.isLoading && $authStore.user) {
+    reservationsPromise = getReservations();
+  }
+
+  async function getNextRides() {
+    const q = query(collection(firestore, 'reservations'), orderBy('pickupTime'), limit(10));
+    const snap = await getDocs(q);
+
+    let _reservations: any[] = [];
+
+    snap.forEach((doc) => {
+      _reservations.push({ id: doc.id, ...doc.data() });
+    });
+
+    return _reservations;
+  }
+
+  let nextRidesPromise;
+
+  $: if (!nextRidesPromise && !$authStore.isLoading && $authStore.user) {
+    nextRidesPromise = getNextRides();
+  }
+</script>
+
+<svelte:head>
+  <title>Reservations | Aerolink</title>
+</svelte:head>
+
+<section class="my-8 mx-auto flex max-w-7xl flex-col space-y-3 px-3 md:flex-row md:space-y-0">
+  <section class="md:order-2 md:ml-8 md:w-2/5">
+    <section class="rounded-lg border border-slate-200 px-5 py-4 shadow-sm">
+      {#if nextRidesPromise}
+        <h5 class="mb-4 text-xl font-bold">Next rides</h5>
+        {#await nextRidesPromise}
+          <p class="text-slate-500">Loading...</p>
+        {:then reservations}
+          {#if reservations}
+            <section class="flex flex-col space-y-2">
+              {#each reservations as reservation (reservation.id)}
+                <ReservationItem {reservation} showPickupTime showTotal />
+              {/each}
+            </section>
+          {/if}
+        {:catch error}
+          <p>{error}</p>
+          <p class="text-slate-500">Couldn't load your reservations.</p>
+        {/await}
+      {/if}
+    </section>
+  </section>
+  <section class="flex-grow rounded-lg md:order-1">
+    {#if reservationsPromise}
+      <h2 class="mb-4 text-2xl font-bold">Reservations</h2>
+      {#await reservationsPromise}
+        <p class="text-slate-500">Loading reservations.</p>
+      {:then reservations}
+        {#if reservations}
+          <section class="flex flex-col space-y-2">
+            {#each reservations as reservation (reservation.id)}
+              <ReservationItem {reservation} showTimestamp showTotal />
+            {/each}
+          </section>
+        {/if}
+      {:catch}
+        <p class="text-slate-500">Couldn't load your reservations.</p>
+      {/await}
+    {/if}
+  </section>
+</section>
