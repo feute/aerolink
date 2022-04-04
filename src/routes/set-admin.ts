068b6eb346aa4663a9e53ba8b418868f6f16a5cf
Angel Gomez <feutet@gmail.com>
2022-04-04 00:47:54 -0400

    Add set-admin endpoint to set admin claim to users

---
+++ src/routes/set-admin.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+import { dev } from '$app/env';
+import { cert, initializeApp, getApps } from 'firebase-admin/app';
+import { getAuth } from 'firebase-admin/auth';
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const post: RequestHandler = async ({ request }) => {
+  if (!dev) {
+    return {
+      status: 400,
+    };
+  }
+
+  const auth = getAuth();
+  const data = await request.formData();
+
+  const uid = String(data.get('uid'));
+
+  if (!uid) {
+    return {
+      status: 400,
+    };
+  }
+
+  const credential = cert(path.resolve('src', 'firebase-admin.json'));
+
+  const apps = getApps();
+
+  if (apps.length === 0) {
+    initializeApp({
+      credential,
+    });
+  }
+
+  await auth.setCustomUserClaims(uid, { admin: true });
+
+  return {
+    status: 200,
+    body: 'ok',
+  };
+};
